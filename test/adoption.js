75d004ca6aba5799179e8dc0a15ebf6d6f354ed8
Jonathan Ato Markin <jona10711@gmail.com>
2023-05-05 15:10:49 +0200

    Tests included

---
+++ test/adoption.js
@@ -0,0 +1,45 @@
+const Adoption = artifacts.require("Adoption");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+
+contract("Adoption", function (accounts) {
+  let adoption;
+
+  let expectedPetId;
+
+  before(async () => {
+    adoption = await Adoption.deployed();
+  });
+
+  describe("Adopting a pet and retrieving account addresses", async () => {
+    before("Adopt a pet using accounts[0]", async () => {
+      await adoption.adopt(8, { from: accounts[0] });
+
+      expectedAdopter = accounts[0];
+    });
+
+    it("Can fetch the address of an owner by pet id", async () => {
+      const adopter = await adoption.adopters(8);
+
+      assert.equal(
+        adopter,
+        expectedAdopter,
+        "The owner of the adopted pet should be the first account."
+      );
+    });
+
+    it("Can fetch the collection of all pet owner's addresses", async () => {
+      const adopters = await adoption.getAdopters();
+
+      assert.equal(
+        adopters[8],
+        expectedAdopter,
+        "The owner of the adopted pet should be in the collection."
+      );
+    });
+  });
+});
